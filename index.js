d15efaa9e92532ddf95513bb7ef79b1e091170f4
ibrahimcs51 <engribrahimkhanokz@gmail.com>
2025-07-23 16:00:22 +0500

    first commit

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use('/',(req,res,next)=>{
 
 // app.get('/user', (req, res) => {
 //   res.send({date: new Date(), message: 'Today date is'});
-// });
+// }); only check sever 
 
 
 app.delete('/user/:id',(req,res)=>{
