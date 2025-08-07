17f69327d8565028d39fb7481b7699c1c458bfd6
ibrahimcs51 <engribrahimkhanokz@gmail.com>
2025-08-07 23:48:52 +0500

    hit git api

--- schema/index.js
+++ schema/index.js
@@ -1,12 +1,12 @@
 import joi from 'joi';
 
-const userSchema=joi.object({
-    name:joi.string().required(),
-    email:joi.string().required(),
-    password:joi.string().required()
+// const userSchema=joi.object({
+//     name:joi.string().required(),
+//     email:joi.string().required(),
+//     password:joi.string().required()
 
 
-})
+// })
 // app.get('/api')
 
 // app.post('/api/user', (req, res) => {
